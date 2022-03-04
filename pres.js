6f9536954d9169dec83dd11914a1ec17797dcf13
Matt Moretti <werebus@gmail.com>
2022-03-04 15:08:09 -0500

    Make "intro" buttons clickable (if ugly)

--- pres.js
+++ pres.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#preso').on('showoff:show', '.slide', function(e) {
     $(e.target).addClass('container');
-    $('a#synchronize').attr('href', '#').removeClass('ui-button').addClass('secondary');
+    $('a#synchronize, a.introjs-button')
+      .attr('href', '#').removeClass('ui-button').addClass('secondary');
   });
 });
