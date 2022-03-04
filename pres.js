9cb581f6c94be3704c0e3168eb6f5ae49245ac47
Matt Moretti <werebus@gmail.com>
2022-03-04 14:50:16 -0500

    Make "sync" button agree with theme

--- pres.js
+++ pres.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('#preso').on('showoff:show', '.slide', function(e) {
     $(e.target).addClass('container');
+    $('a#synchronize').attr('href', '#').removeClass('ui-button').addClass('secondary');
   });
 });
